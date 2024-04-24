f88429a2f1752692070e7596680133fcf518adea
ElenaK <149805926+ElenaKas8@users.noreply.github.com>
2024-04-24 11:35:42 +0200

    task3

--- OlenaKaspruk/tasknext.js
+++ OlenaKaspruk/tasknext.js
@@ -5,3 +5,11 @@ db.users.updateMany(
     { balance: { $gte: 0 } },
     { $mul: { balance: 1.055 } }
 )
+
+
+//Задача. Вывести среднюю продолжительность по всем трекам, но учитывать только треки с продолжительностью от 5 мин (вкл.)
+
+db.tracks.aggregate([
+    { $match: { duration_secs: { $gte: 60 * 5 } } },
+    { $group: { _id: null, avgDuration: { $avg: '$duration_secs' } } }
+])
